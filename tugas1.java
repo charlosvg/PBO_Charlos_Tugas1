1d78505022925f7838e8b7336ee7d152c84b41fb
Charlos Velix Gunawan <71800521+charlosvg@users.noreply.github.com>
2020-10-12 17:43:47 +0800

    Add files via upload

---
+++ tugas1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class tugas1 {
+	public static void main (String[] args){
+		double aa=5,bb=6,cc=0.5,tambah,kali,bagi;
+		
+		tambah=aa+bb;
+		kali=aa*bb*cc;
+		bagi=bb/cc;
+		
+		System.out.println("aa + bb = "+tambah);
+		System.out.println("aa x bb x cc = "+kali);
+		System.out.println("bb / cc + "+bagi);
+	}
+}
